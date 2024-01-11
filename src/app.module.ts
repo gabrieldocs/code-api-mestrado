3532287119bfd719b313ab43b5cbcfd2efc4c34a
gabriel <mydocs.lucas@gmail.com>
2024-01-10 23:19:47 -0300

    modulariza projeto de api

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompletitionsModule } from './completitions/completitions.module';
+import { UserService } from './user.service';
+import { PostService } from './post.service';
+import { PrismaService } from './prisma.service';
+import { GithubModule } from './github/github.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ContainersModule } from './containers/containers.module';
 
 @Module({
-  imports: [CompletitionsModule, ConfigModule.forRoot()],
+  imports: [CompletitionsModule, ConfigModule.forRoot(), ContainersModule, ProjectsModule, GithubModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, PostService, PrismaService],
 })
 export class AppModule {}
--- src/completitions/completitions.service.ts
+++ src/completitions/completitions.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
 import { CreateCompletitionDto } from './dto/create-completition.dto';
 import { UpdateCompletitionDto } from './dto/update-completition.dto';
-
+import { randomUUID } from 'crypto';
+import amqp from "amqplib";
 @Injectable()
 export class CompletitionsService {
   private openai = new OpenAI({
@@ -49,5 +50,56 @@ export class CompletitionsService {
 
   remove(id: number) {
     return `This action removes a #${id} completition`;
+  }
+
+  async publish(payload: any) {
+    const queue = process.env.queue || "completition_topic";
+    const text = {
+      id: randomUUID(),
+      ...payload
+    }
+    let connection: any;
+    try {
+      connection = await amqp.connect(process.env.queue_host || "amqp://localhost")
+
+      const channel = await connection.createChannel()
+
+      await channel.assertQueue(queue, { durable: false })
+      channel.sendToQueue(queue, Buffer.from(JSON.stringify(text)))
+      console.log(" [x] sent  to the queue '%s'", text)
+      await channel.close()
+    } catch (err: any) {
+      console.error(err)
+    } finally {
+      if (connection) await connection.close()
+    }
+  }
+
+  async consumer() {
+    const queue = process.env.queue || "completition_topic";
+    try {
+      const connection = await amqp.connect(process.env.queue_host || "amqp://localhost");
+      const channel = await connection.createChannel();
+
+      process.once("SIGINT", async () => {
+        await channel.close()
+        await connection.close()
+      })
+
+      await channel.assertQueue(queue, { durable: false })
+      await channel.consume(
+        queue,
+        (message) => {
+          if (message) {
+            console.log("[x] received '%s'", JSON.parse(message.content.toString()))
+          }
+        },
+        { noAck: true }
+      )
+
+      console.log("[*] waiting for messages. To exit press CTRL+C")
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
